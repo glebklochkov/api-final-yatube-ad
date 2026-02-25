ca8ccdf5bfa5ac0b0ce5a4522429d7efca9d190e
xx <gleb.klochkov2005@gmail.com>
2026-02-25 19:13:22 +0300

    v1.0

---
+++ api-final-yatube-ad/yatube_api/yatube_api/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import TemplateView
+from rest_framework.authtoken import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('api.urls')),
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.jwt')),
+    path(
+        'redoc/',
+        TemplateView.as_view(template_name='redoc.html'),
+        name='redoc'
+    ),
+]
+
